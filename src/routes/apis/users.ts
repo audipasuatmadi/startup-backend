42391f810785dbe6bdc090e4822c377b562733f8
Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-02-26 14:43:17 +0800

    added login controller

--- src/routes/apis/users.ts
+++ src/routes/apis/users.ts
@@ -1,11 +1,21 @@
 import express from 'express'
 import registerUser from '../../services/user/register';
-import { RegisterRequestBody } from '../../services/user/usertypes';
+import { RegisterRequestBody, LoginCredentials } from '../../services/user/usertypes';
+import loginUser from '../../services/user/login';
 const router = express.Router()
 
 router.post('/', async (req, res) => {
   const requestBody = req.body as RegisterRequestBody
   const result = await registerUser(requestBody)
+  res.status(result.status).json(result.payload)
+})
+
+router.post('/login', async (req, res) => {
+  const requestBody = req.body as LoginCredentials
+  const result = await loginUser(requestBody)
+  console.log('returning result');
+  console.log(result.status)
+  console.log(result.payload)
   res.status(result.status).json(result.payload)
 })
 
